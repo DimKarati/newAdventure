45de0c942012d0ffcded4a8546dc8f76aa1705fe
Dimitri Karati <dimitrikarati@Dimitris-MBP-2.eduroam.gmu.edu>
2022-10-13 11:02:16 -0400

    Connected DB

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const Place = require('./models/place');
 
 //Connecting to MongoDB Cluster
-mongoose.connect('mongodb://localhost:27017/newAdventure');
+mongoose.connect('mongodb+srv://newAdventuredb:zOVtF2Kyh2pbUyZ5@cluster0.mqdwegs.mongodb.net/?retryWrites=true&w=majority');
 
 //Checking if the connection is successfull
 const db = mongoose.connection;
