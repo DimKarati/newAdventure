feced27ac7c4302cda0fd8ada4c63ed3d83abd76
Dimitri Karati <dimitrikarati@Dimitris-MBP-2.eduroam.gmu.edu>
2022-10-13 12:25:54 -0400

    updated

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const Place = require('./models/place');
 
 //Connecting to MongoDB Cluster
-mongoose.connect('mongodb+srv://newAdventuredb:zOVtF2Kyh2pbUyZ5@cluster0.mqdwegs.mongodb.net/new-Adventure');
+mongoose.connect('mongodb+srv://newAdventuredb:zOVtF2Kyh2pbUyZ5@cluster0.mqdwegs.mongodb.net/?retryWrites=true&w=majority');
+
+
 
 //Checking if the connection is successfull
 const db = mongoose.connection;
